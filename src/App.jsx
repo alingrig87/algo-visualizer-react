0b7d526814eb0c1c5f20f83ca074b1a3d71c4744
Alin Grigorescu <alin.grig87@gmail.com>
2024-01-29 22:51:17 +0200

    fix: add / for Home page -- fix for render.com

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import ArraysPage from './pages/ArraysPage';
 import StringsPage from './pages/StringsPage';
@@ -7,15 +7,16 @@ import MapsPage from './pages/MapsPage';
 
 const App = () => {
 	return (
-		<>
+		<HashRouter>
 			<Navbar />
 			<Routes>
+				<Route path="/" element={<div>Home page</div>} />
 				<Route path="/arrays" element={<ArraysPage />} />
 				<Route path="/strings" element={<StringsPage />} />
 				<Route path="/linked-list" element={<LinkedListPage />} />
 				<Route path="/maps" element={<MapsPage />} />
 			</Routes>
-		</>
+		</HashRouter>
 	);
 };
 
